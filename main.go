316dbb569db669d0735e16d1f3d4a0a18f39ddc2
Ramon Teixeira <ramon.nteixeira@gmail.com>
2018-06-23 09:39:24 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", healthCheckHandler)
+	http.ListenAndServe(":5000", nil)
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"alive": true}`)
+}
